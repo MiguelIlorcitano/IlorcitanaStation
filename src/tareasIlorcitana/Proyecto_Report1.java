288bc2cf17abfc6464aba22c37497437a69a455e
Miguel Ángel Carrillo García <Diseo@192.168.0.238>
2020-02-18 15:56:05 +0100

    IlorcitanaStation V1.2.05
    Se han añadido reportes para saber las tareas pendientes de mecánica y operarios.

---
+++ src/tareasIlorcitana/Proyecto_Report1.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareasIlorcitana;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import net.sf.jasperreports.engine.JasperPrint;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import satation.Main;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyecto_Report1 {
+
+    public Proyecto_Report1() {
+        
+    }
+
+    /**
+     * Función principaljj
+     * 
+     * @param i
+     */
+    public void creaReporte(int i) {
+        Connection conn;
+        InputStream inputStream = null;
+        JasperPrint jasperPrint = null;
+        
+        try {
+            if(i==1){
+                inputStream = new FileInputStream("src\\tareasIlorcitana\\report1.jrxml");
+            }else if(i==2){
+                inputStream = new FileInputStream("src\\tareasIlorcitana\\report1.jrxml");
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Proyecto_Report1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            JasperDesign jasperDesign = JRXmlLoader.load(inputStream);
+            JasperReport reporteJasper = JasperCompileManager.compileReport(jasperDesign);
+            JasperPrint mostrarReporte = JasperFillManager.fillReport(reporteJasper, null,conn = DriverManager.getConnection(Main.driver, Main.usuario, Main.clave));
+            JasperViewer.viewReport(mostrarReporte);
+            
+        } catch (JRException ex) {
+            JOptionPane.showMessageDialog(null,"ffff"+ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Proyecto_Report1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- src/tareasIlorcitana/Tareas_Principal.java
+++ src/tareasIlorcitana/Tareas_Principal.java
@@ -298,8 +298,8 @@ public final class Tareas_Principal extends javax.swing.JFrame {
                     }
                     Tabla.setModel(n);
                     Tabla.setDefaultRenderer(Object.class, new Render());
-                    Tabla.getColumnModel().getColumn(0).setPreferredWidth(60);
-                    Tabla.getColumnModel().getColumn(1).setPreferredWidth(60);
+                    Tabla.getColumnModel().getColumn(0).setPreferredWidth(0);
+                    Tabla.getColumnModel().getColumn(1).setPreferredWidth(100);
                     Tabla.getColumnModel().getColumn(2).setPreferredWidth(180);
                     Tabla.getColumnModel().getColumn(3).setPreferredWidth(200);
                     Tabla.getColumnModel().getColumn(4).setPreferredWidth(200);
@@ -480,6 +480,10 @@ public final class Tareas_Principal extends javax.swing.JFrame {
         jlReloj = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jCTipoTarea = new javax.swing.JComboBox<>();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Control de tareas");
@@ -665,7 +669,7 @@ public final class Tareas_Principal extends javax.swing.JFrame {
                 .addGroup(Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jlReloj, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jCNivelPreferencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -691,6 +695,29 @@ public final class Tareas_Principal extends javax.swing.JFrame {
             .addComponent(jScrollPane3)
         );
 
+        jMenu1.setText("Opciones");
+        jMenu1.setFont(new java.awt.Font("3ds Light", 1, 14)); // NOI18N
+
+        jMenuItem1.setText("Pendientes operario");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Pendientes mecánica");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -699,7 +726,7 @@ public final class Tareas_Principal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(Panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(Panel1, javax.swing.GroupLayout.DEFAULT_SIZE, 747, Short.MAX_VALUE)
         );
 
         pack();
@@ -842,6 +869,16 @@ public final class Tareas_Principal extends javax.swing.JFrame {
         contador++;
     }//GEN-LAST:event_jCTipoTareaActionPerformed
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        Proyecto_Report1 p = new Proyecto_Report1();
+        p.creaReporte(2);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        Proyecto_Report1 p = new Proyecto_Report1();
+        p.creaReporte(1);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -886,6 +923,10 @@ public final class Tareas_Principal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JButton jbCreaTarea;
     public javax.swing.JLabel jlReloj;
